c8e9be8c5335ee9e7a297ed2330648e56afc6531
BobErgot <BobErgot@github.com>
2022-11-30 11:27:56 -0500

    Integrate mongodb

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,5 +1,4 @@
-import posts from "./tuits.js";
-let tuits = posts;
+import * as tuitsDao from './tuits-dao.js'
 
 export const currentUser = {
     "userName": "Bobby", "handle": "bob", "avatar": "../../images/profile-image.svg",
@@ -10,38 +9,40 @@ const templateTuit = {
     "topic": "Hot",
     "time": "1 min",
     "liked": false,
+    "likes": 0,
+    "dislikes": 0,
+    "dislike": false,
     "replies": 0,
     "retuits": 0,
-    "likes": 0,
+    "verified": true,
 }
 
-const createTuit = (req, res) => {
+const createTuit = async (req, res) => {
     const newTuit = {
         ...req.body,
         ...templateTuit
     };
-    newTuit._id = (new Date()).getTime()+'';
-    tuits.push(newTuit);
-    res.json(newTuit);
+    const insertedTuit = await tuitsDao.createTuit(newTuit);
+    res.json(insertedTuit);
 }
 
-const findTuits = (req, res) => res.json(tuits);
 
-const updateTuit = (req, res) => {
+const findTuits = async (req, res) => {
+    const tuits = await tuitsDao.findTuits()
+    res.json(tuits);
+}
+
+const updateTuit = async (req, res) => {
     const tuitdIdToUpdate = req.params.tid;
     const updates = req.body;
-    const tuitIndex = tuits.findIndex(
-        (t) => t._id === tuitdIdToUpdate)
-    tuits[tuitIndex] =
-        {...tuits[tuitIndex], ...updates};
-    res.sendStatus(200);
+    const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updates);
+    res.json(status);
 }
 
-const deleteTuit = (req, res) => {
+const deleteTuit = async (req, res) => {
     const tuitdIdToDelete = req.params.tid;
-    tuits = tuits.filter((t) =>
-                             t._id !== tuitdIdToDelete);
-    res.sendStatus(200);
+    const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
+    res.json(status);
 }
 
 export default (app) => {
