d020e901548ef42ab2d4c92aafd39726e1c5eafb
BobErgot <BobErgot@github.com>
2022-11-30 13:32:13 -0500

    Reformat tuit schema

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,9 +1,21 @@
 import mongoose from 'mongoose';
 
 const schema = mongoose.Schema({
+                                   avatar: String,
+                                   userName: String,
+                                   handle: String,
+                                   time: String,
+                                   verified: Boolean,
+                                   topic: String,
                                    tuit: String,
-                                   likes: Number,
+                                   image: String,
+                                   title: String,
                                    liked: Boolean,
+                                   replies: Number,
+                                   retuits: Number,
+                                   likes: Number,
+                                   dislikes: Number,
+                                   dislike: Boolean
                                }
                                , {collection: 'tuits'});
 
